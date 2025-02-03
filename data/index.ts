65241cf3d05cd257b732c80f62b7dfdb8c803a85
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-02-03 15:06:27 +0800

    add header

---
+++ data/index.ts
@@ -0,0 +1,19 @@
+
+//Navigation Links
+export const navItems = [
+    {
+        id: 1,
+        label: 'Home',
+        link: "#home",
+    },
+    {
+        id: 2,
+        label: 'About',
+        link: "#about",
+    },
+    {
+        id: 3,
+        label: 'What we offer',
+        link: "#offer",
+    }
+];
---
+++ lib/types.ts
@@ -0,0 +1,6 @@
+//Button props
+export type ButtonProps = {
+  label: string;
+  link: string;
+  className?: string;
+};
