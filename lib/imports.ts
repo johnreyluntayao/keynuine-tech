297eba871f2365a56ecec4d96bcb3ff14f3a6113
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-02-05 15:19:48 +0800

    add tech stack

--- lib/imports.ts
+++ lib/imports.ts
@@ -4,13 +4,17 @@ export { default as Hero } from '@/components/Hero';
 export { default as Service } from '@/components/Service';
 export { default as Approach } from '@/components/Approach';
 export { default as Contact } from '@/components/Contact';
+export { default as TechStack } from '@/components/TechStack';
 
 //UI Components
 export { default as Button } from '@/components/ui/Button'
+export { default as ApproachCard}  from "@/components/ui/ApproachCard";
 
 //Data
 export {
     navItems,
+    techStack,
+    ApproachCardContent,
 } from '@/data'
 
 //Properties
